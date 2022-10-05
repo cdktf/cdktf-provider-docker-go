ceb3ac46131744a005723167b88f7942d1dc46d6
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-10-05 00:16:36 +0000

    chore(release): v3.0.2

--- docker/registryimage/registryimage_RegistryImageBuildAuthConfigList.go
+++ docker/registryimage/registryimage_RegistryImageBuildAuthConfigList.go
@@ -2,9 +2,9 @@ package registryimage
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-docker-go/docker/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-docker-go/docker/v3/jsii"
 
-	"github.com/hashicorp/cdktf-provider-docker-go/docker/v3/registryimage/internal"
+	"github.com/cdktf/cdktf-provider-docker-go/docker/v3/registryimage/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
